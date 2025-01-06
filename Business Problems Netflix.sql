223705dadcfdaf612052f1e56912376a00b07870
shuvechhadere98 <shuvechha.dere165@gmail.com>
2025-01-06 17:38:57 +0530

    Add files via upload

---
+++ Business Problems Netflix.sql
@@ -0,0 +1,24 @@
+-- 15 Business Problems & Solutions
+
+1. Most Popular Show Type in the Dataset.
+2. Top Countries by Number of Shows.
+3. Trend of Show Release Over the Years.
+4. Find the Most Common Rating for Movies and TV Shows.
+5. Identify the Longest Duration for Movie and TV Show.
+6. Top Directors with Most Content.
+7. Find Each Year and the Average Numbers of Content Release in India on Netflix.
+8. Find Content Added in the Last 5 Years.
+9. Find All the Shows Featuring Robert Downey Jr.
+10. Find All the Movies Released After 2015 in the Action Genre.
+11. Find the Actors hho have Appeared in the Most Shows/Movies.
+12. Find All Content Without a Director.
+13. List All Movies that are Thriller.
+14. Find How Many Movies Actor 'Vikrant Massey' Appeared in the Last 10 Years.
+15. List All TV Shows with More Than 5 Seasons.
+
+
+
+
+
+
+
